40731cb4cc84abc5b95c681f6862e45c09e0724e
Gabriel Lu Junjia <junjia9876@gmail.com>
2025-07-21 16:35:13 +0800

    Idk

---
+++ Assets/InventoryManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject inventory;
+    public InputActionProperty showButton;
+    public TMP_Text message;
+    public Transform head;
+    public float spawnDistance = 2;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        inventory.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+        inventory.transform.LookAt(new Vector3(head.position.x, head.position.y, head.position.z));
+
+    }
+}
