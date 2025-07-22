33688057af0ef7707f49dddccb11095bdf54e9e8
Gabriel Lu Junjia <junjia9876@gmail.com>
2025-07-22 17:14:30 +0800

    werwer
    Inventory

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -14,13 +14,28 @@ public class InventoryManager : MonoBehaviour
     public Transform head;
     public float spawnDistance = 2;
 
+    public bool isInventoryEnabled = false; 
+
 
     // Update is called once per frame
     void Update()
     {
 
-        inventory.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
-        inventory.transform.LookAt(new Vector3(head.position.x, head.position.y, head.position.z));
+        if (!isInventoryEnabled)
+        {
+            inventory.SetActive(false); // Hide until enabled
+            return;
+        }
+
+        inventory.SetActive(true);
+
+        Vector3 forward = new Vector3(head.forward.x, 0, head.forward.z).normalized;
+        Vector3 position = head.position + forward * spawnDistance;
+        position.y -= 0.3f; 
+
+        inventory.transform.position = position;
+        inventory.transform.LookAt(new Vector3(head.position.x, inventory.transform.position.y, head.position.z));
+        inventory.transform.forward *= -1;
 
     }
 }
